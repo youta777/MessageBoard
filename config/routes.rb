4f6f2b3b77b2072645d23bbc0231eda5a77a9e4d
清水耀太 <yousoccer07@yahoo.co.jp>
2019-12-14 18:26:44 +0900

    ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'messages#index' # トップページ
+
+  resources :messages, except: [:index]
+  
+  #　resourcesの中身(RESTful)
+  # get 'messages', to 'messages#index' #一覧
+  # get 'messages/:id', to 'message#show' # 詳細
+  # get 'messages/new', to 'messages#new' # 新規作成
+  # post 'messages', to 'messages#create' # 新規作成メソッド
+  # get 'messages/:id/edit', to 'messages#index' # 編集
+  # put 'messages/:id', to 'messages#update' #上書きメソッド
+  # delete 'messages/:id', to 'messages#destroy' # 削除メソッド
+
+
 end
